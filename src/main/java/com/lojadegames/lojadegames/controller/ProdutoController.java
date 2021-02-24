2f6b3f422693585b70823f5faa91d7b96a3378a1
marcoshakuro <77284884+marcoshakuro@users.noreply.github.com>
2021-02-24 17:43:07 -0300

    implementação dos metodos GetMapping

--- src/main/java/com/lojadegames/lojadegames/controller/ProdutoController.java
+++ src/main/java/com/lojadegames/lojadegames/controller/ProdutoController.java
@@ -1,5 +1,42 @@
 package com.lojadegames.lojadegames.controller;
 
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lojadegames.lojadegames.model.Produto;
+import com.lojadegames.lojadegames.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping ("/produtos")
+@CrossOrigin (origins = "*")
 public class ProdutoController {
+	
+	
+	@Autowired
+	public ProdutoRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getById (@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/produtos/{produtos}")
+	public ResponseEntity<List<Produto>> getByNome (@PathVariable String nome){
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
 
 }
