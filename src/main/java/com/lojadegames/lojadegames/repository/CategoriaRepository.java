f1b1ebb43158b1e8f685a3418ca8de8fbe26df0e
marcoshakuro <77284884+marcoshakuro@users.noreply.github.com>
2021-02-24 10:27:41 -0300

    CategoriaRepository criada

--- src/main/java/com/lojadegames/lojadegames/repository/CategoriaRepository.java
+++ src/main/java/com/lojadegames/lojadegames/repository/CategoriaRepository.java
@@ -1,5 +1,11 @@
 package com.lojadegames.lojadegames.repository;
 
-public interface CategoriaRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.lojadegames.lojadegames.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	public List<Categoria> findAllByNomeContainingIgnoreCase (String nome);
 
 }
