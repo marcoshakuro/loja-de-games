03d73c347adfd48e2d03df06ac074e999427a9c2
marcoshakuro <77284884+marcoshakuro@users.noreply.github.com>
2021-02-24 10:18:01 -0300

    criação categoria model

--- src/main/java/com/lojadegames/lojadegames/model/Categoria.java
+++ src/main/java/com/lojadegames/lojadegames/model/Categoria.java
@@ -1,5 +1,51 @@
 package com.lojadegames.lojadegames.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "categoria")
 public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 200)
+	private String nome;
+	
+	@NotNull
+	boolean status;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
 
 }
