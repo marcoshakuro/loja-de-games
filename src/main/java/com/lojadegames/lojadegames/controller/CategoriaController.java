fdfb983dec880ff3650f76577c1edaa67e876fbc
marcoshakuro <77284884+marcoshakuro@users.noreply.github.com>
2021-02-24 11:06:53 -0300

    criação e inserção dos metodos GetMapping na classe Controller

--- src/main/java/com/lojadegames/lojadegames/controller/CategoriaController.java
+++ src/main/java/com/lojadegames/lojadegames/controller/CategoriaController.java
@@ -1,5 +1,38 @@
 package com.lojadegames.lojadegames.controller;
 
-public class CategoriaController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lojadegames.lojadegames.model.Categoria;
+import com.lojadegames.lojadegames.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categoria")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepository repository; 
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> getAll (){
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> findById (@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/categoria/{categoria}")
+	public ResponseEntity<List<Categoria>> findByName (@PathVariable String nome){
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
 }
