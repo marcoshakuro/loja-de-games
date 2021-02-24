e5379aebf6940cc9b7eff8fe0ac2a2090e0152bc
marcoshakuro <77284884+marcoshakuro@users.noreply.github.com>
2021-02-24 12:15:08 -0300

    criação da classe Produto e sua abstração

--- src/main/java/com/lojadegames/lojadegames/model/Produto.java
+++ src/main/java/com/lojadegames/lojadegames/model/Produto.java
@@ -1,5 +1,96 @@
 package com.lojadegames.lojadegames.model;
 
-public class Produto {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 300)
+	private String nome;
+	
+	@NotNull
+	private float preco;
+	
+	@NotNull
+	private int quantidade;
+	
+	@NotNull
+	private boolean status;
+	
+	
+	private Categoria categoria;
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public float getPreco() {
+		return preco;
+	}
+
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+
+	public boolean isStatus() {
+		return status;
+	}
+
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
 }
